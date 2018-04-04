cdeb034c6f2dfc3da8a452c9b139beb346704c4f
LeeSD90 <lee.doyle90@gmail.com>
2018-04-04 19:20:24 +0200

    Improved Boards draw method

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,11 @@
 class Board
-	attr_accessor :cells
+require 'colorize'
+	attr_accessor :cells, :size
 
 	Point = Struct.new(:x, :y)
 
 	def initialize()
+		@size = 7
 		@cells = []
 		8.times{|i|
 			ary = []
@@ -16,17 +18,38 @@ class Board
 
 	def draw()
 		puts "\n\n"
+		alternate = true
 		@cells.reverse.each do |row|
-			print (row[0].x + 1).to_s + " |"
+			print " " * size
+			if(alternate)
+				@cells.length.times do |i| print i%2 == 0 ? (" " * size).colorize(:background => :white) : (" " * size).colorize(:background => :red) end
+			else
+				@cells.length.times do |i| print i%2 == 0 ? (" " * size).colorize(:background => :red) : (" " * size).colorize(:background => :white) end
+			end
+
 			row.each do |cell|
-				print "______|"
+				if cell.y == 0 then print "\n" + (" " * (size/2)) + (row[0].x + 1).to_s + (" " * (size/2)) end
+				if(alternate)
+					print cell.y%2 == 0 ? (" " * size).colorize(:background => :white) : (" " * size).colorize(:background => :red)
+				else
+					print cell.y%2 == 0 ? (" " * size).colorize(:background => :red) : (" " * size).colorize(:background => :white)
+				end
 			end
 			puts
-			puts "\n\n" unless row[0].x == 0
+			print (" " * size)
+			if(alternate)
+				@cells.length.times do |i| print i%2 == 0 ? (" " * size).colorize(:background => :white) : (" " * size).colorize(:background => :red) end
+			else
+				@cells.length.times do |i| print i%2 == 0 ? (" " * size).colorize(:background => :red) : (" " * size).colorize(:background => :white) end
+			end
+			puts
+			alternate ^= true
+			#puts "\n" unless row[0].x == 0
 		end
 		puts "\n"
+		print " " * (size-3)
 		('a'..'h').each do |l|
-			print "      " + l
+			print (" " * (size-1)) + l
 		end
 	end
 
